a232627ec7b330c85e062c2602c4ecf117c4e7b5
Sonia  Nicas <sonia.d.nicas@gmail.com>
2018-05-04 23:30:08 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -18,7 +18,7 @@ class Transfer
       @sender.balance = @sender.balance - @amount
       @receiver.balance = @receiver.balance + @amount
       @status = "complete"
-  end
+
   if valid? == false
     @status = "rejected"
      "Transaction rejected. Please check your account balance."
