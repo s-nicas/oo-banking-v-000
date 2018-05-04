856e75fc305550a4c7b5724a4a35c8f9157e1cf4
Sonia  Nicas <sonia.d.nicas@gmail.com>
2018-05-04 23:26:57 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -18,7 +18,6 @@ class Transfer
       @sender.balance = @sender.balance - @amount
       @receiver.balance = @receiver.balance + @amount
       @status = "complete"
-    # end
   end
   if sender.valid? == false
     @status = "rejected"
