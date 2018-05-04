5ddffc0ec6f7fbc3377c1970f76884c82ef6956b
Sonia  Nicas <sonia.d.nicas@gmail.com>
2018-05-04 23:32:22 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -26,6 +26,7 @@ class Transfer
 end
 
 def reverse_transfer
+  if status == "complete"
   @sender.balance = @sender.balance + @amount
   @receiver.balance = @receiver.balance - @amount
   @staus = "reversed"
